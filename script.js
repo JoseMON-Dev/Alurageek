c294a9553c5489047a6abb1732f5bb949a65355e
jose <montesotero8@gmail.com>
2022-10-31 20:33:47 -0500

    haciendo funcionar formulario seccion principal

---
+++ script.js
@@ -0,0 +1,11 @@
+const formulario = document.querySelector("#form");
+const btnmail = document.querySelector("#truco")
+formulario.addEventListener("submit", submit)
+
+function submit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    console.log(form.get("Nombre"))
+    btnmail.setAttribute("href",`mailto:montesotero8@gmail.com?subject=${form.get("Nombre")}${form.get("mensaje")}&body=`)
+    btnmail.click()
+}
