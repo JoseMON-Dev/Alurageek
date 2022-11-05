85858bd9437bc31f796a995a076e0f556fe9c15e
jose <montesotero8@gmail.com>
2022-11-04 20:02:26 -0500

    agregando seccion de agregar producto y menu de busqueda

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function submit(event){
     btnmail.setAttribute("href",`mailto:montesotero8@gmail.com?subject=${form.get("Nombre")}&body=${form.get("mensaje")}`)
     btnmail.click()
 }
+
+
+const lupa = document.querySelector("#logo_lupa_1");
+const cabezara = document.querySelector("#cabezera")
+lupa.addEventListener("click", onclick)
+const menu_buscar = document.querySelector("#menu_buscar");
+const arrowheader = document.querySelector("#arrowheader");
+arrowheader.addEventListener("click",remove)
+
+function onclick(event){
+    cabezara.style.display= 'none' ;
+    menu_buscar.style.display='initial';
+}
+
+function remove(event){
+    console.log(event)
+    menu_buscar.style.display= 'none' ;
+    cabezara.style.display='flex';
+
+}
